745b35eef5f098ce5df3060b7e6abb794c84933d
Md Mohosin Ali <mohosinali075@gmail.com>
2025-08-30 02:18:12 +0600

    Project setup done

---
+++ src/routes/index.ts
@@ -0,0 +1,32 @@
+import { UserRoutes } from '../app/modules/user/user.route'
+import { AuthRoutes } from '../app/modules/auth/auth.route'
+import express, { Router } from 'express'
+import { NotificationRoutes } from '../app/modules/notifications/notifications.route'
+import { PublicRoutes } from '../app/modules/public/public.route'
+import { SupportRoutes } from '../app/modules/support/support.route'
+import { OnboardingscreenRoutes } from '../app/modules/onboardingscreen/onboardingscreen.route'
+import { ReviewRoutes } from '../app/modules/review/review.route'
+import { CategoryRoutes } from '../app/modules/category/category.route'
+
+const router = express.Router()
+
+const apiRoutes: { path: string; route: Router }[] = [
+  { path: '/user', route: UserRoutes },
+  { path: '/auth', route: AuthRoutes },
+
+  { path: '/notifications', route: NotificationRoutes },
+
+  { path: '/public', route: PublicRoutes },
+
+  { path: '/support', route: SupportRoutes },
+  { path: '/onboardingscreen', route: OnboardingscreenRoutes },
+  { path: '/review', route: ReviewRoutes },
+
+  { path: '/category', route: CategoryRoutes },
+]
+
+apiRoutes.forEach(route => {
+  router.use(route.path, route.route)
+})
+
+export default router
